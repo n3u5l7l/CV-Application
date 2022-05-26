612a711b80435feb69b2a580edd2507c20ed9fb6
MingMing <greeteaz567@gmail.com>
2022-05-26 03:50:09 -0500

    Added placeholder variables

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,6 +7,15 @@ import {ReactComponent as TwitterLogo} from "../assets/twitter.svg";
 class Info extends React.Component{
     constructor(props){
         super(props);
+
+        this.firstName = "First Name";
+        this.lastName = "Last Name";
+        this.currRole = "Current Role";
+        this.phoneNum = "xxx-xxx-xxxx";
+        this.linkedIn ="linkedin.com/example";
+        this.email = "example@gmail.com";
+        this.twitter = "@example";
+        this.description =  "Tell me about yourself";
     }
 
     render(){
@@ -14,30 +23,30 @@ class Info extends React.Component{
         return (
             <div className="info">
                 <div className="name">
-                    <h2 contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.state.firstName}</h2>
-                    <h2 contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.state.lastName}</h2>
+                    <h2 contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.firstName}</h2>
+                    <h2 contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.lastName}</h2>
                 </div>
-                <h3 className="role" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.state.currRole}</h3>
+                <h3 className="role" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.currRole}</h3>
                 <div className="contacts">
                     <div className="phone-section">
                         <PhoneLogo alt="phone"/>
-                        <div className="phoneNumber" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.state.phoneNum}</div>
+                        <div className="phoneNumber" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.phoneNum}</div>
                     </div>
                     <div className="linkedIn-section">
                         <LinkedInLogo alt="linkedin"/>
-                        <div className="linkedIn" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.state.linkedIn}</div>
+                        <div className="linkedIn" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.linkedIn}</div>
                     </div>
                     <div className="email-section">
                         <EmailLogo alt="email" />
-                        <div className="email" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.state.email}</div>
+                        <div className="email" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.email}</div>
                     </div>
                     <div className="twitter-section">
                         <TwitterLogo alt="twitter" />
-                        <div className="twitter" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.state.twitter}</div>
+                        <div className="twitter" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>{this.twitter}</div>
                     </div>
                 </div>
                 <div className="aboutMe" contentEditable={this.props.previewMode === "ON" ? "false": "true"}>
-                    {this.state.description}
+                    {this.description}
                 </div>
             </div>
         )
