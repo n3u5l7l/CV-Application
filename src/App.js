c106174f8bc5692d6d46f42da05290a0ccefbe7d
MingMing <greeteaz567@gmail.com>
2022-05-25 19:50:15 -0500

    Added preview option

--- src/App.js
+++ src/App.js
@@ -7,22 +7,39 @@ import Skill from "./components/Skill";
 class App extends Component{
   constructor(){
     super();
+
+    this.state = {
+      previewMode: "OFF",
+      previewStyle: {
+        background: "",
+        opacity:"",
+      }
+    }
   }
 
+  handleClick = (e) => {
+    const previewOff = this.state.previewMode === "OFF" ? true: false;
+
+    this.setState({
+      previewMode: previewOff ? "ON": "OFF",
+      previewStyle: previewOff ? {background: "green", opacity: "1"} : {background: "", opacity: ""} 
+    });
+
+  }
   render(){
     return (
     <div className="App">
       <div className="title">
         <h1>CV-Application</h1>
-        <div className="previewMode">
-          on off
-        </div>
+        <button className="previewMode" style = {this.state.previewStyle} onClick={this.handleClick}>
+          Preview {this.state.previewMode}
+        </button>
       </div>
       <div className="AppContent">
-        <Info />
-        <Experience />
-        <Education />
-        <Skill />
+        <Info previewMode = {this.state.previewMode}/>
+        <Experience previewMode = {this.state.previewMode}/>
+        <Education previewMode = {this.state.previewMode}/>
+        <Skill previewMode = {this.state.previewMode}/>
       </div>
     </div>
     );
